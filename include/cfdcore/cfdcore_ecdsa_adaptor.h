fb5ebcdd4800d15cdc3ea44552f3e96b222c4b7f
k-matsuzawa <tangrambs803@gmail.com>
2021-05-24 11:54:10 +0900

    fix: remove old format support.

--- include/cfdcore/cfdcore_ecdsa_adaptor.h
+++ include/cfdcore/cfdcore_ecdsa_adaptor.h
@@ -17,8 +17,6 @@ using cfd::core::ByteData256;
 using cfd::core::Privkey;
 using cfd::core::Pubkey;
 
-class AdaptorProof;
-
 /**
  * @brief An adaptor signature.
  *
@@ -29,10 +27,6 @@ class CFD_CORE_EXPORT AdaptorSignature {
    * @brief Size of an adaptor signature.
    */
   static constexpr uint32_t kAdaptorSignatureSize = 162;
-  /**
-   * @brief Size of an adaptor signature minimum data.
-   */
-  static constexpr uint32_t kAdaptorSignatureMinimumSize = 65;
 
   /**
    * @brief Create an adaptor signature over the given message using the given
@@ -58,15 +52,6 @@ class CFD_CORE_EXPORT AdaptorSignature {
    * @param data the data representing the adaptor signature
    */
   explicit AdaptorSignature(const std::string &data);
-
-  /**
-   * @brief Construct a new Adaptor Signature object from ByteData & proof.
-   *
-   * @param signature the adaptor signature
-   * @param proof the data representing the adaptor signature proof
-   */
-  explicit AdaptorSignature(
-      const AdaptorSignature &signature, const AdaptorProof &proof);
 
   /**
    * @brief Copy constructor.
@@ -123,13 +108,6 @@ class CFD_CORE_EXPORT AdaptorSignature {
   ByteData GetData() const;
 
   /**
-   * @brief Get the ByteData object
-   *
-   * @return ByteData
-   */
-  ByteData GetFullyData() const;
-
-  /**
    * @brief Constructer.
    */
   AdaptorSignature() {}
@@ -147,140 +125,6 @@ class CFD_CORE_EXPORT AdaptorSignature {
    *
    */
   ByteData data_;
-  /**
-   * @brief The fully signature data
-   */
-  ByteData sig_data_;
-};
-
-/**
- * @brief A proof for an adaptor signature.
- *
- * @deprecated This class is deprecated due to refactoring.
- */
-class CFD_CORE_EXPORT AdaptorProof {
- public:
-  /**
-   * @brief The size of an adaptor proof.
-   *
-   */
-  static constexpr uint32_t kAdaptorProofSize = 97;
-  /**
-   * @brief Construct a new Adaptor Proof from ByteData
-   *
-   * @param data the data representing the proof
-   */
-  explicit AdaptorProof(const ByteData &data);
-  /**
-   * @brief Construct a new Adaptor Proof object from a string
-   *
-   * @param data the data representing the proof
-   */
-  explicit AdaptorProof(const std::string &data);
-
-  /**
-   * @brief Get the underlying ByteData object.
-   *
-   * @return ByteData
-   */
-  ByteData GetData() const;
-
-  /**
-   * @brief Constructer.
-   */
-  AdaptorProof() {}
-
-  /**
-   * @brief check valid.
-   * @retval true
-   * @retval false
-   */
-  bool IsValid() const;
-
- private:
-  /**
-   * @brief The underlying data
-   *
-   */
-  ByteData data_;
-};
-
-/**
- * @brief A pair of an adaptor signature together with the proof attached to it.
- *
- * @deprecated This struct is deprecated due to refactoring.
- */
-struct AdaptorPair {
-  /**
-   * @brief An adaptor signature
-   *
-   */
-  AdaptorSignature signature;
-  /**
-   * @brief An adaptor proof
-   *
-   */
-  AdaptorProof proof;
-};
-
-/**
- * @brief This class contain utility functions to work with adaptor signatures.
- * @deprecated This class is deprecated due to refactoring.
- */
-class CFD_CORE_EXPORT AdaptorUtil {
- public:
-  /**
-   * @brief Create an adaptor signature over the given message using the given
-   * private key. Returns an AdaptorPair of the adaptor signature and its proof.
-   *
-   * @param msg the message to create the signature for.
-   * @param sk the secret key to create the signature with.
-   * @param encryption_key the adaptor to adapt the signature with.
-   * @return AdaptorPair
-   */
-  static AdaptorPair Sign(
-      const ByteData256 &msg, const Privkey &sk, const Pubkey &encryption_key);
-
-  /**
-   * @brief "Decrypt" an adaptor signature using the provided secret, returning
-   * an ecdsa signature in compact format.
-   *
-   * @param signature the adaptor signature
-   * @param adaptor_secret the secret
-   * @return ByteData
-   */
-  static ByteData Adapt(
-      const AdaptorSignature &signature, const Privkey &adaptor_secret);
-
-  /**
-   * @brief Extract an adaptor secret from an ECDSA signature for a given
-   * adaptor signature.
-   *
-   * @param adaptor_signature the adaptor signature
-   * @param signature the ECDSA signature
-   * @param encryption_key the adaptor for the signature
-   * @return Privkey
-   */
-  static Privkey ExtractSecret(
-      const AdaptorSignature &adaptor_signature, const ByteData &signature,
-      const Pubkey &encryption_key);
-
-  /**
-   * @brief Verify that an adaptor proof is valid with respect to a given
-   * adaptor signature, adaptor, message and public key.
-   *
-   * @param adaptor_signature
-   * @param proof
-   * @param encryption_key
-   * @param msg
-   * @param pubkey
-   * @retval true
-   * @retval false
-   */
-  static bool Verify(
-      const AdaptorSignature &adaptor_signature, const AdaptorProof &proof,
-      const Pubkey &encryption_key, const ByteData256 &msg,
-      const Pubkey &pubkey);
 };
 
 }  // namespace core
--- src/cfdcore_ecdsa_adaptor.cpp
+++ src/cfdcore_ecdsa_adaptor.cpp
@@ -24,18 +24,8 @@ using cfd::core::CfdException;
 // AdaptorSignature
 // ------------------------
 AdaptorSignature::AdaptorSignature(const ByteData &data)
-    : data_(data), sig_data_() {
-  if (data_.GetDataSize() == AdaptorSignature::kAdaptorSignatureMinimumSize) {
-    std::vector<uint8_t> sig_arr(AdaptorSignature::kAdaptorSignatureSize);
-    auto sig_arr_ptr = sig_arr.data();
-    memset(sig_arr_ptr, 0, sig_arr.size());
-
-    auto sig_data = data.GetBytes();
-    auto sig_ptr = sig_data.data();
-    memcpy(&sig_arr_ptr[0], &sig_ptr[0], 33);
-    memcpy(&sig_arr_ptr[66], &sig_ptr[33], 32);
-    sig_data_ = ByteData(sig_arr);
-  } else if (data_.GetDataSize() != AdaptorSignature::kAdaptorSignatureSize) {
+    : data_(data) {
+  if (data_.GetDataSize() != AdaptorSignature::kAdaptorSignatureSize) {
     throw CfdException(
         CfdError::kCfdIllegalArgumentError, "Invalid adaptor signature data.");
   }
@@ -44,47 +34,13 @@ AdaptorSignature::AdaptorSignature(const ByteData &data)
 AdaptorSignature::AdaptorSignature(const std::string &data)
     : AdaptorSignature(ByteData(data)) {}
 
-AdaptorSignature::AdaptorSignature(
-    const AdaptorSignature &signature, const AdaptorProof &proof)
-    : data_(), sig_data_() {
-  if ((!signature.IsValid()) || (!proof.IsValid())) {
-    throw CfdException(
-        CfdError::kCfdIllegalArgumentError,
-        "Invalid adaptor signature or proof.");
-  }
-
-  if (signature.data_.GetDataSize() ==
-      AdaptorSignature::kAdaptorSignatureSize) {
-    data_ = signature.data_;
-  } else {
-    std::vector<uint8_t> sig_arr(AdaptorSignature::kAdaptorSignatureSize);
-    auto sig_arr_ptr = sig_arr.data();
-
-    std::vector<uint8_t> sig_data = signature.GetData().GetBytes();
-    std::vector<uint8_t> proof_data = proof.GetData().GetBytes();
-
-    auto sig_ptr = sig_data.data();
-    auto proof_ptr = proof_data.data();
-    memcpy(&sig_arr_ptr[0], &sig_ptr[0], 33);
-    memcpy(&sig_arr_ptr[33], &proof_ptr[0], 33);
-    memcpy(&sig_arr_ptr[66], &sig_ptr[33], 32);
-    memcpy(&sig_arr_ptr[98], &proof_ptr[33], 32);
-    memcpy(&sig_arr_ptr[130], &proof_ptr[65], 32);
-    data_ = ByteData(sig_arr);
-  }
-}
-
 AdaptorSignature::AdaptorSignature(const AdaptorSignature &signature) {
   data_ = signature.data_;
-  sig_data_ = signature.sig_data_;
 }
 
 AdaptorSignature &AdaptorSignature::operator=(
     const AdaptorSignature &signature) & {
-  if (this != &signature) {
-    data_ = signature.data_;
-    sig_data_ = signature.sig_data_;
-  }
+  if (this != &signature) data_ = signature.data_;
   return *this;
 }
 
@@ -112,10 +68,9 @@ ByteData AdaptorSignature::Decrypt(const Privkey &sk) const {
   }
   auto ctx = wally_get_secp_context();
   secp256k1_ecdsa_signature secp_signature;
-  auto &data = sig_data_.IsEmpty() ? data_ : sig_data_;
   auto ret = secp256k1_ecdsa_adaptor_decrypt(
       ctx, &secp_signature, sk.GetData().GetBytes().data(),
-      data.GetBytes().data());
+      data_.GetBytes().data());
 
   if (ret != 1) {
     throw CfdException(
@@ -143,9 +98,8 @@ Privkey AdaptorSignature::Recover(
   std::vector<uint8_t> secret(Privkey::kPrivkeySize);
   auto secp_sig = ParseSignature(signature);
   auto secp_adaptor = ParsePubkey(encryption_key);
-  auto &data = sig_data_.IsEmpty() ? data_ : sig_data_;
   auto ret = secp256k1_ecdsa_adaptor_recover(
-      ctx, secret.data(), &secp_sig, data.GetBytes().data(), &secp_adaptor);
+      ctx, secret.data(), &secp_sig, data_.GetBytes().data(), &secp_adaptor);
 
   if (ret != 1) {
     throw CfdException(
@@ -165,95 +119,15 @@ bool AdaptorSignature::Verify(
   auto ctx = wally_get_secp_context();
   auto secp_pubkey = ParsePubkey(pubkey);
   auto secp_adaptor = ParsePubkey(encryption_key);
-  auto &data = sig_data_.IsEmpty() ? data_ : sig_data_;
   return secp256k1_ecdsa_adaptor_verify(
-             ctx, data.GetBytes().data(), &secp_pubkey, msg.GetBytes().data(),
+             ctx, data_.GetBytes().data(), &secp_pubkey, msg.GetBytes().data(),
              &secp_adaptor) == 1;
 }
 
 ByteData AdaptorSignature::GetData() const { return data_; }
 
-ByteData AdaptorSignature::GetFullyData() const {
-  return sig_data_.IsEmpty() ? data_ : sig_data_;
-}
-
 bool AdaptorSignature::IsValid() const {
-  if ((data_.GetDataSize() == AdaptorSignature::kAdaptorSignatureSize) &&
-      (sig_data_.IsEmpty())) {
-    return true;
-  } else if (
-      (sig_data_.GetDataSize() == AdaptorSignature::kAdaptorSignatureSize) &&
-      (data_.GetDataSize() ==
-       AdaptorSignature::kAdaptorSignatureMinimumSize)) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-// ------------------------
-// AdaptorProof
-// ------------------------
-AdaptorProof::AdaptorProof(const ByteData &data) : data_(data) {
-  if ((data_.GetDataSize()) != AdaptorProof::kAdaptorProofSize) {
-    throw CfdException(
-        CfdError::kCfdIllegalArgumentError, "Invalid adaptor proof data.");
-  }
-}
-
-ByteData AdaptorProof::GetData() const { return data_; }
-
-AdaptorProof::AdaptorProof(const std::string &data)
-    : AdaptorProof(ByteData(data)) {}
-
-bool AdaptorProof::IsValid() const {
-  return (data_.GetDataSize() == AdaptorProof::kAdaptorProofSize);
-}
-
-// ------------------------
-// AdaptorUtil
-// ------------------------
-AdaptorPair AdaptorUtil::Sign(
-    const ByteData256 &msg, const Privkey &sk, const Pubkey &encryption_key) {
-  auto sig = AdaptorSignature::Encrypt(msg, sk, encryption_key);
-  auto sig_arr = sig.GetData().GetBytes();
-  auto sig_arr_ptr = sig_arr.data();
-
-  std::vector<uint8_t> sig_data(
-      AdaptorSignature::kAdaptorSignatureMinimumSize);
-  std::vector<uint8_t> proof_data(AdaptorProof::kAdaptorProofSize);
-
-  auto sig_ptr = sig_data.data();
-  auto proof_ptr = proof_data.data();
-  memcpy(&sig_ptr[0], &sig_arr_ptr[0], 33);
-  memcpy(&proof_ptr[0], &sig_arr_ptr[33], 33);
-  memcpy(&sig_ptr[33], &sig_arr_ptr[66], 32);
-  memcpy(&proof_ptr[33], &sig_arr_ptr[98], 32);
-  memcpy(&proof_ptr[65], &sig_arr_ptr[130], 32);
-
-  AdaptorPair pair;
-  pair.signature = AdaptorSignature(ByteData(sig_data));
-  pair.proof = AdaptorProof(ByteData(proof_data));
-  return pair;
-}
-
-ByteData AdaptorUtil::Adapt(
-    const AdaptorSignature &adaptor_signature, const Privkey &sk) {
-  return adaptor_signature.Decrypt(sk);
-}
-
-Privkey AdaptorUtil::ExtractSecret(
-    const AdaptorSignature &adaptor_sig, const ByteData &signature,
-    const Pubkey &encryption_key) {
-  return adaptor_sig.Recover(signature, encryption_key);
-}
-
-bool AdaptorUtil::Verify(
-    const AdaptorSignature &adaptor_sig, const AdaptorProof &proof,
-    const Pubkey &encryption_key, const ByteData256 &msg,
-    const Pubkey &pubkey) {
-  auto new_sig = AdaptorSignature(adaptor_sig, proof);
-  return new_sig.Verify(msg, pubkey, encryption_key);
+  return data_.GetDataSize() == AdaptorSignature::kAdaptorSignatureSize;
 }
 
 }  // namespace core
